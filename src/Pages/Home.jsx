1501adc66b779dad10a9c42dc7165a3fb6d886ac
Saran Ramalingam <saranramalingam@AGLS-MLT-497.local>
2022-03-22 17:20:19 +0530

    covid19

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import { React, useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import axios from "axios";
+import Bar from "../Pages/Bar";
 
 export default function Home() {
   const [country, setCountry] = useState([]);
-  const [activecase, setActiveCase] = useState();
   const fetchdata = async () => {
     try {
       const Countrydata = await axios.get(
@@ -21,6 +21,20 @@ export default function Home() {
     fetchdata();
   }, []);
 
+  const Total = country.reduce((current, previous) => {
+    return Number(current) + Number(previous.TotalCases);
+  }, 0);
+
+  const Recover = country.reduce((current, previous) => {
+    return Number(current) + Number(previous.TotalRecovered);
+  }, 0);
+  const Active = country.reduce((current, previous) => {
+    return Number(current) + Number(previous.ActiveCases);
+  }, 0);
+  const Deaths = country.reduce((current, previous) => {
+    return Number(current) + Number(previous.TotalDeaths);
+  }, 0);
+
   return (
     <div>
       <Navbar />
@@ -32,37 +46,28 @@ export default function Home() {
           <br></br>
           <div className="bg-secondary bg-opacity-25 container">
             <div className=" fs-4 text-primary">Total Cases</div>
-            <h2>
-              {country.reduce((current, previous) => {
-                return Number(current) + Number(previous.TotalCases);
-              }, 0)}
-            </h2>
+            <h2>{Total}</h2>
             <br></br>
             <div className=" fs-4 text-success">Recovered Cases</div>
-            <h2>
-              {country.reduce((current, previous) => {
-                return Number(current) + Number(previous.TotalRecovered);
-              }, 0)}
-            </h2>
+            <h2>{Recover}</h2>
             <br></br>
             <div className=" fs-4 text-warning">Active Cases</div>
-            <h2>
-              {country.reduce((current, previous) => {
-                return Number(current) + Number(previous.ActiveCases);
-              }, 0)}
-            </h2>
+            <h2>{Active}</h2>
             <br></br>
             <div className=" fs-4 text-danger">Deaths</div>
-            <h2>
-              {country.reduce((current, previous) => {
-                return Number(current) + Number(previous.TotalDeaths);
-              }, 0)}
-            </h2>
+            <h2>{Deaths}</h2>
             <br></br>
           </div>
         </div>
         <div className="col-md-6 col-lg-6 col-xl-6 col-sm-12">
-          <div className="h1">Covid-19</div>
+          <div className="h1">
+            <Bar
+              Total={Total}
+              Death={Deaths}
+              Active={Active}
+              Recover={Recover}
+            ></Bar>
+          </div>
         </div>
       </div>
     </div>
