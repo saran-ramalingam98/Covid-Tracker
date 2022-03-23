37d0ab8b86d80dde9ee91f2aa7c684980be7d4fe
saran-ramalingam98 <98377456+saran-ramalingam98@users.noreply.github.com>
2022-03-23 11:51:47 +0530

    Update Bar.jsx

--- src/Pages/Bar.jsx
+++ src/Pages/Bar.jsx
@@ -1,28 +1,28 @@
-import React from "react";
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from "victory";
+// import React from "react";
+// import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from "victory";
 
-export default function Bar(props) {
-  var a = Number(props.Total);
-  var b = Number(props.Recover);
-  var c = Number(props.Active);
-  var d = Number(props.Death);
+// export default function Bar(props) {
+//   var a = Number(props.Total);
+//   var b = Number(props.Recover);
+//   var c = Number(props.Active);
+//   var d = Number(props.Death);
 
-  const data = [
-    { quarter: 1, earnings: a },
-    { quarter: 2, earnings: b },
-    { quarter: 3, earnings: c },
-    { quarter: 4, earnings: d },
-  ];
+//   const data = [
+//     { quarter: 1, earnings: a },
+//     { quarter: 2, earnings: b },
+//     { quarter: 3, earnings: c },
+//     { quarter: 4, earnings: d },
+//   ];
 
-  return (
-    <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
-      <VictoryAxis
-        tickValues={[1, 2, 3, 4]}
-        tickFormat={["Total", "Recovered", "Active", "Deaths"]}
-        style={{ fontSize: "1em" }}
-      />
-      <VictoryAxis dependentAxis tickFormat={(x) => `${x / 10000000}Cr`} />
-      <VictoryBar data={data} x="quarter" y="earnings" />
-    </VictoryChart>
-  );
-}
+//   return (
+//     <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
+//       <VictoryAxis
+//         tickValues={[1, 2, 3, 4]}
+//         tickFormat={["Total", "Recovered", "Active", "Deaths"]}
+//         style={{ fontSize: "1em" }}
+//       />
+//       <VictoryAxis dependentAxis tickFormat={(x) => `${x / 10000000}Cr`} />
+//       <VictoryBar data={data} x="quarter" y="earnings" />
+//     </VictoryChart>
+//   );
+// }
